100986e609b9b3a83461f61dcada825efc5cd6e2
Martin Kühl <martin.kuehl@gmail.com>
2018-03-27 10:38:19 +0200

    Add tests for matching

---
+++ test/test_match_extension/faucet.config.js
@@ -0,0 +1,13 @@
+"use strict";
+let path = require("path");
+
+module.exports = {
+	static: [{
+		source: "./src",
+		target: "./dist",
+		filter: path => path.endsWith(".txt")
+	}],
+	plugins: {
+		"static": path.resolve("../..")
+	}
+};
