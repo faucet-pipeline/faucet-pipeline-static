b5f35be10c15ea11b111fc8e368a9b0de00163e9
Lucas Dohmen <lucas@dohmen.io>
2025-03-08 21:57:02 +0100

    Apply FND's suggestions
    * Improvements to FileFinder
    * Doc improvement
    * fileName => filename
    * determineCompactors
    * Inline processFiles
    * Code organization
    * Remove last `then`
    * Inline isDotfile

---
+++ lib/types.ts
@@ -0,0 +1,62 @@
+// faucet-pipeline-core types
+export interface FaucetPlugin<T> {
+	(config: T[], assetManager: AssetManager, options: FaucetPluginOptions): FaucetPluginFunc
+}
+
+export interface FaucetPluginFunc {
+	(filepaths: string[]): Promise<unknown>
+}
+
+export interface FaucetPluginOptions {
+	browsers?: string[],
+	sourcemaps?: boolean,
+	compact?: boolean
+}
+
+export interface AssetManager {
+	resolvePath: (path: string, opts?: ResolvePathOpts) => string
+		writeFile: (targetPath: string, content: Buffer, options: WriteFileOpts) => Promise<unknown>
+}
+
+export interface ResolvePathOpts {
+	enforceRelative?: boolean
+}
+
+export interface WriteFileOpts {
+	targetDir: string,
+	fingerprint?: boolean
+}
+
+// faucet-pipeline-static types
+export interface Config {
+	source: string,
+	target: string,
+	targetDir: string,
+	fingerprint?: boolean,
+	compact?: CompactorMap,
+	assetManager: AssetManager,
+	filter?: (filename: string) => boolean
+}
+
+export interface CompactorMap {
+	[fileExtension: string]: Compactor
+}
+
+export interface Compactor {
+	(contact: Buffer): Promise<Buffer>
+}
+
+export interface ProcessFile {
+	source: string,
+	target: string,
+	targetDir: string,
+	fingerprint?: boolean,
+	compactors: CompactorMap,
+	assetManager: AssetManager,
+	filter?: (filename: string) => boolean
+}
+
+export interface FileFinderOptions {
+	skipDotfiles: boolean,
+	filter?: (filename: string) => boolean
+}
