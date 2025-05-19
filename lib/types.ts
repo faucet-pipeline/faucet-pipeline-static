cd249cb16f81f5fbd3a0b9745a8d029c1e2e865b
Lucas Dohmen <lucas@dohmen.io>
2025-05-19 09:36:08 +0200

    Use the types.ts from core

--- lib/types.ts
+++ lib/types.ts
@@ -1,33 +1,5 @@
-// faucet-pipeline-core types
-export interface FaucetPlugin<T> {
-	(config: T[], assetManager: AssetManager, options: FaucetPluginOptions): FaucetPluginFunc
-}
+import { AssetManager } from "faucet-pipeline-core/lib/types.ts"
 
-export interface FaucetPluginFunc {
-	(filepaths: string[]): Promise<unknown>
-}
-
-export interface FaucetPluginOptions {
-	browsers?: string[],
-	sourcemaps?: boolean,
-	compact?: boolean
-}
-
-export interface AssetManager {
-	resolvePath: (path: string, opts?: ResolvePathOpts) => string
-	writeFile: (targetPath: string, content: Buffer, options: WriteFileOpts) => Promise<unknown>
-}
-
-export interface ResolvePathOpts {
-	enforceRelative?: boolean
-}
-
-export interface WriteFileOpts {
-	targetDir: string,
-	fingerprint?: boolean
-}
-
-// faucet-pipeline-static types
 export interface Config {
 	source: string,
 	target: string,
